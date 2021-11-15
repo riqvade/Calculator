b3571cb4b6fd9575162ebcef2af058a95dd1cadf
riqvade <40538535+riqvade@users.noreply.github.com>
2021-11-15 14:20:10 +0700

    Добавил оператор switch case, добавил обработку ошибки деления на ноль.

--- Calculator/CalculatorModel.cs
+++ Calculator/CalculatorModel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace Calculator
 {
@@ -10,32 +11,52 @@ namespace Calculator
     {
         public CalculatorModel()
         {
-            
+
         }
 
         public object Calculate(int operand1, int operand2, char operation)
         {
-            if (operation.Equals('+'))
+            switch (operation)
             {
-                return operand1 + operand2;
-            }
+                case ('+'):
+                    return Sum(operand1, operand2);
 
-            if (operation.Equals('-'))
-            {
-                return operand1 - operand2;
-            }
+                case ('-'):
+                    return Subtraction(operand1, operand2);
 
-            if (operation.Equals('*'))
-            {
-                return operand1 * operand2;
-            }
+                case ('*'):
+                    return Multiplication(operand1, operand2);
 
-            if (operation.Equals('/'))
-            {
-                return operand1 / operand2;
+                case ('/'):
+                    return Division(operand1, operand2);
             }
 
             return null;
+        }
+
+        private int Sum (int operand1, int operand2)
+        {
+            return operand1 + operand2;
+        }
+
+        private int Subtraction(int operand1, int operand2)
+        {
+            return operand1 - operand2;
+        }
+
+        private int Multiplication(int operand1, int operand2)
+        {
+            return operand1 * operand2;
+        }
+
+        private object Division(int operand1, int operand2)
+        {
+            if (operand2.Equals(0))
+            {
+                return "Ошибка!";
+            }
+
+            return (double)operand1 / (double)operand2;
         }
     }
 }
